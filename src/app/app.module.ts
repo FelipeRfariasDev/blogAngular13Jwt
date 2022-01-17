306e714a14de762e548dd8c4fb99bb61ffc5641d
felipe <felipe.farias.php@gmail.com>
2022-01-17 01:04:44 -0300

    criando os componentes menu, login, posts, home e definindo as rotas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PostsComponent } from './posts/posts.component';
+import { MenuComponent } from './menu/menu.component';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PostsComponent,
+    MenuComponent,
+    HomeComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
