b7b84bb36d0536084509f9dd694329fccb96b177
felipe <felipe.farias.php@gmail.com>
2022-02-27 18:29:57 -0300

    listar comentários relacionado a um post, botão de excluir e opção para adicionar um novo comentário.

--- src/app/posts/components/detail-posts/detail-posts.component.ts
+++ src/app/posts/components/detail-posts/detail-posts.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, TemplateRef } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { BsModalRef, BsModalService } from 'ngx-bootstrap/modal';
+import { ToastrService } from 'ngx-toastr';
 import { Post } from '../../model/post';
 import { PostsService } from '../../service/posts.service';
 
@@ -9,12 +13,49 @@ import { PostsService } from '../../service/posts.service';
 export class DetailPostsComponent implements OnInit {
 
   post?:Post;
+  modalRef: BsModalRef | undefined;
 
-  constructor(private postservice: PostsService) { 
+  form = new FormGroup({
+    descricao: new FormControl('', Validators.required)
+  });
+
+  constructor(private postservice: PostsService,
+    private router: Router,
+    private toastr:ToastrService,
+    private modalService: BsModalService) { 
     this.post = postservice.getPost();
   }
 
   ngOnInit(): void {
+  }
+
+  addComentario(){
+    alert("adicionar um comentário");
+  }
+  openModal(template: TemplateRef<any>) {
+    this.modalRef = this.modalService.show(template, {class: 'modal-sm'});
+  }
+  deleteComentario(comentario: any, template:TemplateRef<any>) {
+    this.openModal(template);
+  }
+
+  confirm(): void {
+    /*
+    this.postsService.delete(this.post!).subscribe((response:any)=>{
+      if(response.success){
+        this.getPagesList(this.link_page_atual);
+        this.toastr.success(response.message,"Sucesso");
+        return;
+      }
+      this.toastr.error(response.message,"Erro");
+    });
+    
+    */
+    this.modalRef?.hide();
+  }
+ 
+  decline(): void {
+    this.modalRef?.hide();
   }
 
 }
